cfd248a1948ad24a49fdb44cb48b441efef7d5d7
Adam Cohen <acohen89@binghamton.edu>
2021-07-09 14:41:42 -0700

    Changed styling and added glog page

--- app.js
+++ app.js
@@ -4,15 +4,28 @@ const port = 3000;
 
 const server = http.createServer(function(req, res) { 
     res.writeHead(200, {"Content-Type": "text/html"})
-    fs.readFile("index.html", function(error, data){
-        if(error){
-            res.writeHead(404);
-            res.write("Error: file not found");
-        } else {
-            res.write(data);
-        }
-        res.end();
-    });
+    if(req.url == "/gLog"){
+        fs.readFile("Pages/goodLogin.html", function(error, data){
+            if(error){ 
+                res.writeHead(404);
+                res.write("Error: file not found");
+            } else {
+                res.write(data);
+            }
+            res.end();
+        });
+    } else {
+        fs.readFile("Pages/index.html", function(error, data){
+            if(error){ 
+                res.writeHead(404);
+                res.write("Error: file not found");
+            } else {
+                res.write(data);
+            }
+            res.end();
+        });
+    }
+    
 });
 
 server.listen(port, function(error){
