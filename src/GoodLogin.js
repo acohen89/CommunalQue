e987984a1780f419161de6aed70d3aa1e77ee693
Adam Cohen <acohen89@binghamton.edu>
2021-07-14 11:43:43 -0700

    Webpages work

---
+++ src/GoodLogin.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function GoodLogin() {
+  return (
+    <body>
+        <p>Sucessful Login!</p>
+    </body >
+  );
+}
+
+export default GoodLogin;
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Home() {
+  return (
+    <body>
+        <p>Welcome To Communal Que!</p>
+    </body >
+  );
+}
+
+export default Home;
