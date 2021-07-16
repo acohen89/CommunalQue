1d2145c8e689ec135caffeba72aa6f130c0ecfe2
Adam Cohen <acohen89@binghamton.edu>
2021-07-15 22:12:00 -0700

    Added Switch

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import Home from "./Home";
 import GoodLogin from "./GoodLogin";
-import  {BrowserRouter as Router, Route } from "react-router-dom";
+import  {BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
    <Router>
      <div className = "App">
-       <Route path = "/home" component ={Home}/>
-       {/* <Route path = "/" component ={Home}/> */}
-       <Route path = "/gLog" component ={GoodLogin}/>
+       <Switch>
+        <Route path = "/" exact component ={Home}/>
+        <Route path = "/home" component ={Home}/>
+        <Route path = "/gLog" component ={GoodLogin}/>
+       </Switch>
+
      </div>
    </Router>
   );
