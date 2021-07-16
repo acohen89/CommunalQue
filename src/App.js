b721f29b8eb3bd4c39a31029ac86506d31f234ea
Adam Cohen <acohen89@binghamton.edu>
2021-07-15 22:05:13 -0700

    remoed / route

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
    <Router>
      <div className = "App">
        <Route path = "/home" component ={Home}/>
-       <Route path = "/" component ={Home}/>
+       {/* <Route path = "/" component ={Home}/> */}
        <Route path = "/gLog" component ={GoodLogin}/>
      </div>
    </Router>
