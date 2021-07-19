bd2b88e9d2acf9098f40860151d32937de71dc20
Adam Cohen <acohen89@binghamton.edu>
2021-07-19 13:54:19 -0700

    Set up for testing

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,9 +2,9 @@ import React, { useEffect } from "react";
 import './styles/Home.css';
 
 
-const CLIENT_ID = "161c6b8ff4a9482d962700e1b41cac89"; // QueQue id
+const CLIENT_ID = "35135547562945148a4c9129b244dfe8"; // Testing iD
 const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
-const REDIRECT_URL_AFTER_LOGIN = "https://communalque.hostman.site/gLog";
+const REDIRECT_URL_AFTER_LOGIN = "http://localhost:5000/gLog";
 const SPACE_DELIMITER = "%20";
 const SCOPES = [
   "app-remote-control",
