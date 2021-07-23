7172b7fed4eb73f7c3a3097c9a9f0fc5cbd22b88
Adam Cohen <acohen89@binghamton.edu>
2021-07-23 14:57:50 -0700

    got sample data from firebase

--- src/components/MainQue.js
+++ src/components/MainQue.js
@@ -4,8 +4,10 @@ import firebase from "./firesbase";
 import {WEB_URL} from "./Home";
 import Button from "./Button";
 import InQue from './InQue';
+const TEST_HASH = "testHash";
 const db = firebase.firestore();
 const hash = getHash(6);
+const docRef = db.collection("Active Ques").doc(TEST_HASH);
 
 
 
@@ -14,16 +16,53 @@ function MainQue() {
   const [songs, setSongs] = useState([{id: "123kf21", title: "Piano Man", artist: "Billy Joel"}, 
   {id: "198213da", title: "She's Always A Woman", artist: "Billy Joel"}]);
   
+  const refresh = () => {
+    console.log("refreshed")
+    docRef.get().then((doc) => {
+      if (doc.exists) {
+          setSongs(songs => (songs = doc.data().songs))
+        } else { console.log("No such document!");}
+      }).catch((error) => {
+      console.log("Error getting document:", error);
+  });
+  }
+
   return (
     <>
     <h>
-       <p> Que With Id: {hash} </p>
+      useEffect(() => {
+          <p> Que With Id: {hash} </p>
+      }, [])
        <Button text="End Que" onClick={endQue} />
     </h>
-       <InQue songs = {songs}/>
-
+      <InQue songs = {songs}/>
+      <Button text="Refresh" onClick={refresh}/> 
     </>
   );
+}
+const endQue = () => {
+  // migrate data to past ques collection 
+  // go to home page
+  window.location.href = WEB_URL;
+  
+}
+function getHash(){
+  const hash = makeHash(6);
+  console.log("Fire")
+  db.collection("Active Ques").doc(hash).set({
+    Songs: []
+  })
+  .then((docRef) => {
+    console.log("Document written sucesfully");
+    return hash;
+    
+  })
+  .catch((error) => {
+    console.error("Error adding document: ", error);
+    return hash;
+  });
+  return hash;
+  
 }
 function makeHash(length) {
     var result = '';
@@ -33,34 +72,6 @@ function makeHash(length) {
       result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
    return result;
-}
-const endQue = () => {
-    // migrate data to past ques collection 
-    // go to home page
-    window.location.href = WEB_URL;
-
-}
-function getHash(){
-  const hash = makeHash(6);
-  console.log("Fire")
-    db.collection("Active Ques").add({
-        QueId: hash,
-        Songs: []
-    })
-    .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
-        const refId = docRef.id;
-        console.log(".then")
-        return hash;
-
-    })
-    .catch((error) => {
-        console.error("Error adding document: ", error);
-        console.log(".catch")
-        return hash;
-    });
-    return hash;
-    
 }
 
 
